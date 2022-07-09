0838f74bbacfde118233d04c3590ad1ac0a1f783
Elena <gudulina@mail.ru>
2022-07-09 13:18:28 +0700

    Add task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 
+Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+int Degree(int a, int b)
+{
+    int mult = a;
+
+    for (int i = 1; i < b; i++)
+    {
+        mult *= a;
+    }
+    return mult;
+}
+
+Console.WriteLine(Degree(3, 5));
+Console.WriteLine(Degree(2, 4));
