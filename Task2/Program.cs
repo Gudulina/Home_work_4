c4e09f6588e222d39843b5461922f37cb1037939
Elena <gudulina@mail.ru>
2022-07-09 14:29:28 +0700

    Add task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int Summa(int a)
+{
+    int sum = 0;
+    while (a > 0)
+    {
+        sum += a % 10;
+        a /= 10;
+    }   
+    return sum;
+}
+
+Console.WriteLine(Summa(452));
+Console.WriteLine(Summa(82));
+Console.WriteLine(Summa(9012));
